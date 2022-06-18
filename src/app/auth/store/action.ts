ee0c04f36b7fb6cfe53771fe786bb11d1451fb04
thappatanchanphen <thappatanchanphen@gmail.com>
2022-06-18 16:36:48 +0700

    Add: shared type interface, auth type interdface

--- src/app/auth/store/action.ts
+++ src/app/auth/store/action.ts
@@ -1,11 +1,9 @@
 import { createAction, props } from '@ngrx/store';
+
+import { RegisterRequestInterface } from '../types/registerRequest.interface';
 import { ActionTypes } from './actionTypes';
 
 export const registerAction = createAction(
   ActionTypes.REGISTER,
-  props<{
-    username: string;
-    password: string;
-    email: string;
-  }>()
+  props<{request: RegisterRequestInterface}>()
 );
