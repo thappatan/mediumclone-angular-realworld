96e8d65c84e4f250bb176873fff8c513f2dfb5c4
thappatanchanphen <thappatanchanphen@gmail.com>
2022-06-22 15:00:21 +0700

    Add: login component

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -11,11 +11,16 @@ import { AuthService } from './services/auth.service';
 import { RegisterEffect } from './store/effects/register.effect';
 import { BackendErrorMessagesModule } from '../shared/modules/backendErrorMessages/backendErrorMessages.module';
 import { PersistanceService } from '../shared/services/persitance.service';
+import { LoginComponent } from './components/login/login.component';
 
 const routes = [
   {
     path: 'register',
     component: RegisterComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
   },
 ];
 
@@ -26,9 +31,9 @@ const routes = [
     ReactiveFormsModule,
     StoreModule.forFeature('auth', reducers),
     EffectsModule.forFeature([RegisterEffect]),
-    BackendErrorMessagesModule
+    BackendErrorMessagesModule,
   ],
-  declarations: [RegisterComponent],
-  providers: [AuthService, PersistanceService]
+  declarations: [RegisterComponent, LoginComponent],
+  providers: [AuthService, PersistanceService],
 })
 export class AuthModule {}
