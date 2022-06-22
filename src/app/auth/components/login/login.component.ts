e984e735cdc02957abca8f16b164ae1229e7b8f5
thappatanchanphen <thappatanchanphen@gmail.com>
2022-06-22 15:11:52 +0700

    Update: register form group, form control to login form

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'mc-login-component',
   templateUrl: './login.component.html',
   styleUrls: ['login.component.scss'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  form!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.initialzeForms();
+  }
+
+  initialzeForms() {
+    this.form = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.form.value, this.form.valid);
+  }
 }
